927f1246ef7f7283b49e0a272272b0408a541d5b
Achal Singhal <achals2002@gmail.com>
2023-01-18 17:24:57 +0530

    initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
-import './globals.css'
+/* eslint-disable @next/next/no-head-element */
+import '@/styles/globals.css'
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html>
+      <head></head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
